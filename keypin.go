aff5694e55bf22a20ce7126d2a89d6b94fc2d6fe
Brumens <68474266+Brum3ns@users.noreply.github.com>
2021-10-12 14:38:56 +0100

    Add files via upload

--- keypin.go
+++ keypin.go
@@ -2,24 +2,46 @@
 package main
 
 import (
+	"os"
 	"fmt"
-	"flag"
+	"flag"	
+	"bufio"
+	"strings"
 	//"net/http"
 )
 
+
+
 //User arguments - [User Input]:
-var domain = flag.String("domain", "xx", "Target to test on")
+var domain = flag.String("d", "", "Domain to test")
+var path = flag.String("p", "", "Path to bypass")
+var bypass = flag.String("b", "all", `Bypass technique to use`)
+var header = flag.String("hc", "", "Header/cookie to add in requests")
+var method = flag.String("m", "GET", "Method to use")
+var agent = flag.String("a", "/home/kali/project/keypin/db/rua.txt", "user agent to use")
+var output = flag.String("o", "", "Save output")
+
+//Random user agent file & list:
+var rua_file, rua_err = os.Open("/home/kali/project/keypin/db/rua.txt")
+var lst_rua = make([]string,0)
 
 
+
+//Run keypin with it's linked functions:
 func main() {
-	var banner = fmt.Println(`
-		[BANNER]
-		`)
+	//Banner Design & option(parse) display ("-h, --help"):
+	banner()
+	flag.Parse()
+	
+	//Configure KeyPin:
+	rua()
+
+	config()
+
+
+
 
 	//Enable user input args to all functions that will run in "main":
-	flag.Parse()
-
-	protocol()
 
 
 
@@ -42,6 +64,50 @@ func main() {
 
 	fmt.Println(":: Process done.")
 }
+
+
+//Banner design:
+func banner() {
+	fmt.Println(`
+	
+	[BANNER]
+	
+- Version: v1.0
+- Author: Brumens
+	`)
+}
+
+
+func config() {
+
+	fmt.Print("x")
+
+	//Information & user configure output:
+	fmt.Print(strings.Repeat("-", 60),"\n",
+	"· Bypass: \n",
+	"· Target: \n",
+	"· Path: \n",
+	"· Threads: \n",
+	"· Timeout: \n",
+	"· Delay: \n",
+	"· Header: \n",
+	"· Cookie: \n",
+	"· User-Agents: [",len(lst_rua),"] Lines\n",
+	strings.Repeat("-", 60),"\n")
+
+}
+
+//User agent to list:
+func rua() {
+
+	scanner := bufio.NewScanner(rua_file)
+    
+    //Adding all lines(user-agents) to a list:
+    for scanner.Scan() {
+        lst_rua = append(lst_rua, scanner.Text())
+    }
+}
+
 
 //HTTP protocol bypass:
 func protocol() {
