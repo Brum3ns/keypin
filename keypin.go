03e0d0f48d68f461c3ed02e0cccb352e0cd7dbd9
Brumens <68474266+Brum3ns@users.noreply.github.com>
2021-12-23 16:45:18 +0000

    Update keypin.go

--- keypin.go
+++ keypin.go
@@ -493,12 +493,16 @@ func config_Check(opt *options, st *storage) {
 
 // [] User agent to list:
 func setup_lists(st *storage) {
-
+	//Execute "whoami" to get the user name and to make a $HOME variable
+	WHOAMI, _ := exec.Command("whoami").Output()
+	path := fmt.Sprintf("/home/%v/.config/keypin/conf/db/", string(WHOAMI))
+	path = strings.Replace(path, "\n", "", 1)
+	
 	lst_files := []string{"rua.txt", "path_bypass.txt", "verb_bypass.txt", "headers_bypass.txt"}
 
 	for i := 0; i < len(lst_files); i++ {
 
-		file_input, _ := os.Open("$HOME/.config/keypin/conf/db/"+lst_files[i])
+		file_input, _ := os.Open(path+lst_files[i])
 		scanner := bufio.NewScanner(file_input)
 
    		for scanner.Scan() {
